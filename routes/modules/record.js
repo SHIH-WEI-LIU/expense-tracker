fe9610818bb96bb52bec55ce5fa189ebf9f44fec
SHIH-WEI-LIU <lsw850718@yahoo.com>
2022-09-04 16:24:22 +0800

    modify edit route problems

--- routes/modules/record.js
+++ routes/modules/record.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/Record')
-
+const Category = require('../../models/Category')
 // routes setting
 //create
 router.get('/new', (req, res) => {
@@ -17,8 +17,8 @@ router.post('/', (req, res) => {
 
 
 //編輯資料頁面的路由
-router.get('/:id/edit', (req, res) => {
-  const _id = req.params.id
+router.get('/:_id/edit', (req, res) => {
+  const _id = req.params._id
   const userId = req.user._id
   return Record.findOne({ _id, userId })
     .lean()
@@ -26,13 +26,15 @@ router.get('/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 //修改特定資料的路由
-router.put('/:id', (req, res) => {
-  const id = req.params.id
+router.put('/:_id', (req, res) => {
+  const _id = req.params._id
   const userId = req.user._id
-  return Record.findOneAndUpdate({ id, userId, ...req.body }) //找到對應的資料後整個一起更新
+  return Record.findOneAndUpdate({ _id, userId }, {...req.body}) //找到對應的資料後整個一起更新
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+
+
 
 //刪除的路由
 router.delete('/:id', (req, res) => {
@@ -41,11 +43,6 @@ router.delete('/:id', (req, res) => {
   return Record.findOneAndDelete({ _id, userId })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-})
-
-//chart
-router.get('/chart', (req, res) => {
-  return res.render('chart')
 })
 
 
